710d88d9c091e550d8ff2093663c7bbadd52c8de
raysiagian <anggasiagian17@gmail.com>
2024-05-21 08:41:15 +0700

    fixing leaderboard

--- app/Http/Controllers/Api/LeaderboardController.php
+++ app/Http/Controllers/Api/LeaderboardController.php
@@ -20,11 +20,11 @@ class LeaderboardController extends Controller
         $scorePosttestQuery = ScorePosttest::joinSub($subquery, 'min_scores', function ($join) {
                 $join->on('score_posttest.id_ScorePosttest', '=', 'min_scores.min_id');
             })
-            ->select('min_scores.id_user', DB::raw('SUM(score_posttest.score) as total_score_posttest'))
+            ->select('min_scores.id_user', DB::raw('COALESCE(SUM(score_posttest.score), 0) as total_score_posttest'))
             ->groupBy('min_scores.id_user');
 
         // Subquery untuk mendapatkan ID lencana pengguna terkecil dan total lencana unik untuk setiap pengguna
-        $badgeSubquery = LencanaUser::select('id_user', DB::raw('MIN(id_lencanaPengguna) as min_lencana, COUNT(DISTINCT id_bagde) as total_badges'))
+        $badgeSubquery = LencanaUser::select('id_user', DB::raw('COALESCE(MIN(id_lencanaPengguna), 0) as min_lencana, COALESCE(COUNT(DISTINCT id_bagde), 0) as total_badges'))
             ->groupBy('id_user');
 
         // Gabungkan dengan query utama untuk mendapatkan data lengkap leaderboard
@@ -32,7 +32,9 @@ class LeaderboardController extends Controller
             ->mergeBindings($scorePosttestQuery->getQuery()) // Gunakan getQuery() untuk mendapatkan query builder
             ->leftJoinSub($badgeSubquery, 'badge_counts', 'score_posttests.id_user', '=', 'badge_counts.id_user')
             ->join('user', 'score_posttests.id_user', '=', 'user.id_user') // Join dengan tabel user
-            ->select('user.name', 'score_posttests.total_score_posttest', 'badge_counts.total_badges') // Pilih kolom nama dari tabel user
+            ->select('user.name', DB::raw('COALESCE(score_posttests.total_score_posttest, 0) as total_score_posttest'), DB::raw('COALESCE(badge_counts.total_badges, 0) as total_badges')) // Pilih kolom nama dari tabel user
+            ->orderBy('total_score_posttest', 'desc') // Urutkan berdasarkan total_score_posttest dari yang terbesar ke yang terkecil
+            ->limit(10) // Ambil hanya 10 data teratas
             ->get();
 
         return response()->json(['data' => $leaderboardData]);
